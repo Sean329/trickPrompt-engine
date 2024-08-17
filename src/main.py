c42a04123fd37ae1b6a10feb15e8bc93937f454e
Xiangyu Xu <xuxiangy@amazon.com>
2024-08-16 19:07:43 -0700

    added bot results sentimentV2 ++ updated prompt

--- src/main.py
+++ src/main.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
         # project_id = 'whalefall'
         # project_id = 'od-contracts'
         # project_id = 'nextgen'
-        project_id = 'alkimiya'
+        project_id = 'sentimentV2'
         project_path = ''
         project = Project(project_id, projects[project_id])
         
